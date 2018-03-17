c224467328d859eea92c5b9768d414ab10867875
slewis <slewis@composent.com>
2018-03-17 14:11:09 -0700

    Additional fixes for bug https://bugs.eclipse.org/bugs/show_bug.cgi?id=532205
    Add support for OSGi Remote Services 1.1 intents

--- bundles/org.eclipse.ecf.provider.jgroups/src/org/eclipse/ecf/provider/jgroups/container/JGroupsContainerInstantiator.java
+++ bundles/org.eclipse.ecf.provider.jgroups/src/org/eclipse/ecf/provider/jgroups/container/JGroupsContainerInstantiator.java
@@ -1,8 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Composent, Inc. and others. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Composent, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.ecf.provider.jgroups.container;
 
-import java.io.InputStream;
-import java.net.URL;
+import java.net.URI;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -10,12 +18,16 @@ import org.eclipse.ecf.core.ContainerCreateException;
 import org.eclipse.ecf.core.ContainerTypeDescription;
 import org.eclipse.ecf.core.IContainer;
 import org.eclipse.ecf.core.identity.IDFactory;
+import org.eclipse.ecf.core.provider.ContainerIntentException;
 import org.eclipse.ecf.core.util.ECFException;
 import org.eclipse.ecf.provider.generic.SOContainerConfig;
 import org.eclipse.ecf.provider.jgroups.identity.JGroupsID;
 import org.eclipse.ecf.provider.jgroups.identity.JGroupsNamespace;
 import org.eclipse.ecf.remoteservice.provider.RemoteServiceContainerInstantiator;
+import org.jgroups.Global;
 import org.jgroups.JChannel;
+import org.jgroups.conf.ConfiguratorFactory;
+import org.jgroups.conf.ProtocolStackConfigurator;
 
 public class JGroupsContainerInstantiator extends RemoteServiceContainerInstantiator {
 
@@ -25,11 +37,8 @@ public class JGroupsContainerInstantiator extends RemoteServiceContainerInstanti
 	public static final String JGROUPS_ID_PROP = "id";
 	public static final String JGROUPS_MANAGER_ID_DEFAULT = JGroupsNamespace.INSTANCE.getScheme()
 			+ ":ecf.jgroups.defaultGroup";
-	public static final String JGROUPS_CHANNEL_CONFIG_URL = "channelConfigUrl";
-	public static final String JGROUPS_CHANNEL_CONFIG_STRING = "channelConfigString";
-	public static final String JGROUPS_CHANNEL_CONFIG_INPUTSTREAM = "channelConfigInputStream";
-	public static final String JGROUPS_CHANNEL_CONFIG_CHANNEL = "channelJChannel";
-
+	public static final String JGROUPS_CHANNEL_CONFIG_STRING = "channelConfigProperty";
+	
 	public JGroupsContainerInstantiator() {
 		super();
 		exporterConfigs.add(JGROUPS_MANAGER_CONFIG);
@@ -40,6 +49,11 @@ public class JGroupsContainerInstantiator extends RemoteServiceContainerInstanti
 				Arrays.asList(new String[] { JGROUPS_CLIENT_CONFIG, JGROUPS_MANAGER_CONFIG }));
 	}
 
+	@Override
+	protected boolean supportsOSGIAsyncIntent(ContainerTypeDescription description) {
+		return true;
+	}
+	
 	@Override
 	public IContainer createInstance(ContainerTypeDescription description, Object[] parameters)
 			throws ContainerCreateException {
@@ -59,7 +73,7 @@ public class JGroupsContainerInstantiator extends RemoteServiceContainerInstanti
 			}
 			if (newID == null)
 				return super.createInstance(description, parameters);
-			return createJGroupsContainer(newID, null, channel, description.isServer());
+			return createJGroupsContainer(description, newID, null, channel);
 		} catch (final Exception e) {
 			ContainerCreateException cce = new ContainerCreateException("Exception creating jgroups manager container",
 					e);
@@ -68,9 +82,19 @@ public class JGroupsContainerInstantiator extends RemoteServiceContainerInstanti
 		}
 	}
 
-	protected IContainer createJGroupsContainer(JGroupsID newID, Map<String, ?> parameters, JChannel channel,
-			boolean server) throws ECFException {
-		if (server) {
+	protected IContainer createJGroupsContainer(ContainerTypeDescription description, JGroupsID newID, Map<String, ?> parameters, JChannel channel) throws ECFException {
+		// If jgroups bindAddress is set use that otherwise localhost
+		String hostname = System.getProperty("jgroups.bind_addr","localhost");
+		URI uri = null;
+		try {
+			uri = new URI("jgroups://"+hostname);
+		} catch (Exception e) {
+			// won't happen
+		}
+		// Now check intents
+		checkOSGIIntents(description, uri, (parameters==null)?new HashMap<String,Object>():parameters);
+		// If passed then return appropriate container instance
+		if (description.isServer()) {
 			newID = (JGroupsID) getIDParameterValue(JGroupsNamespace.INSTANCE, parameters, JGROUPS_ID_PROP,
 					JGROUPS_MANAGER_ID_DEFAULT);
 			JGroupsManagerContainer manager = new JGroupsManagerContainer(new SOContainerConfig(newID), channel);
@@ -83,31 +107,23 @@ public class JGroupsContainerInstantiator extends RemoteServiceContainerInstanti
 		}
 	}
 
-	protected JChannel getChannelFromParameters(Map<String, ?> parameters, boolean server) throws Exception {
-		URL configURL = getParameterValue(parameters, JGroupsContainerInstantiator.JGROUPS_CHANNEL_CONFIG_URL,
-				URL.class, null);
-		if (configURL != null)
-			return new JChannel(configURL);
+	protected JChannel getChannelFromParameters(ContainerTypeDescription description, Map<String, ?> parameters) throws Exception {
+		ProtocolStackConfigurator configurator = null;
 		String configString = getParameterValue(parameters, JGROUPS_CHANNEL_CONFIG_STRING, String.class, null);
-		if (configString != null)
-			return new JChannel(configString);
-		InputStream ins = getParameterValue(parameters, JGROUPS_CHANNEL_CONFIG_INPUTSTREAM, InputStream.class, null);
-		if (ins != null)
-			return new JChannel(ins);
-		JChannel jChannel = getParameterValue(parameters, JGROUPS_CHANNEL_CONFIG_CHANNEL, JChannel.class, null);
-		if (jChannel != null)
-			return jChannel;
-		return null;
+		if (configString != null) 
+			configurator = ConfiguratorFactory.getStackConfigurator(configString);
+		configurator = ConfiguratorFactory.getStackConfigurator(Global.DEFAULT_PROTOCOL_STACK);
+		return new JChannel(configurator);
 	}
 
 	@Override
 	public IContainer createInstance(ContainerTypeDescription description, Map<String, ?> parameters)
 			throws ContainerCreateException {
-		boolean server = description.isServer();
 		try {
-			return createJGroupsContainer(null, parameters, getChannelFromParameters(parameters, server),
-					description.isServer());
+			return createJGroupsContainer(description, null, parameters, getChannelFromParameters(description, parameters));
 		} catch (Exception e) {
+			if (e instanceof ContainerIntentException) 
+				throw (ContainerIntentException) e;
 			ContainerCreateException cce = new ContainerCreateException("Exception creating jgroups manager container",
 					e);
 			cce.setStackTrace(e.getStackTrace());
